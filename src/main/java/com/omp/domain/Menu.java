007b92cd6a99393b941674dd3455400a0fc1df41
sunjong0214 <rlatjswhd8200@gmail.com>
2025-01-13 02:06:23 +0900

    feat(Menu): Menu Entity 구현

--- src/main/java/com/omp/domain/Menu.java
+++ src/main/java/com/omp/domain/Menu.java
@@ -1,4 +1,21 @@
 package com.omp.domain;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class Menu {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "menu_id")
+    private Long id;
+    private String name;
+    private Integer price;
+    private String description;
 }
