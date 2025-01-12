919340b89dba5025f2629b54cd6fb73e4f6e624a
sunjong0214 <rlatjswhd8200@gmail.com>
2025-01-13 02:42:55 +0900

    feat(Menu): Menu Entity 구현

--- src/main/java/com/omp/domain/Order.java
+++ src/main/java/com/omp/domain/Order.java
@@ -1,4 +1,21 @@
 package com.omp.domain;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ORDERS")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "ORDER_ID")
+    private Long id;
+    private Long deliveryId;
+    private Long paymentId;
+    private Long shopId;
 }
