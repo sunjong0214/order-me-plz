40cbe1ea170a1786f94cd9e208a1b3c16d64e3a2
sunjong0214 <rlatjswhd8200@gmail.com>
2025-01-13 01:57:25 +0900

    feat(User): User Entity 구현

--- src/main/java/com/omp/domain/User.java
+++ src/main/java/com/omp/domain/User.java
@@ -1,4 +1,21 @@
 package com.omp.domain;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class User {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
 }
