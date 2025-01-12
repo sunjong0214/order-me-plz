ae2cb8a7b76ac11d462cb451ce82d4a47e585d00
sunjong0214 <rlatjswhd8200@gmail.com>
2025-01-13 01:58:20 +0900

    fix: id 전략 수정

--- src/main/java/com/omp/domain/Delivery.java
+++ src/main/java/com/omp/domain/Delivery.java
@@ -1,9 +1,12 @@
 package com.omp.domain;
 
-import static jakarta.persistence.EnumType.*;
+import static jakarta.persistence.EnumType.STRING;
+import static jakarta.persistence.GenerationType.IDENTITY;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -11,6 +14,8 @@ import jakarta.persistence.Table;
 @Table
 public class Delivery {
     @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "delivery_id")
     private Long id;
     @Enumerated(STRING)
     private DeliveryStatus status;
