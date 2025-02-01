862a3a013e03f064701f2ea26f4d702f2d867b0d
sunjong0214 <rlatjswhd8200@gmail.com>
2025-02-01 17:07:38 +0900

    refactor

--- src/main/java/com/omp/shop/ShopController.java
+++ src/main/java/com/omp/shop/ShopController.java
@@ -1,15 +1,15 @@
 package com.omp.shop;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 @RequestMapping("/api/v1/shop")
 public class ShopController {
     private final ShopService shopService;
