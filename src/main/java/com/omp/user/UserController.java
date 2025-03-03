1c8ee14b75b6984ef246fb0759e2e4b3acd24aa3
sunjong0214 <rlatjswhd8200@gmail.com>
2025-03-04 04:42:48 +0900

    feat: user api 구현

--- src/main/java/com/omp/user/UserController.java
+++ src/main/java/com/omp/user/UserController.java
@@ -1,7 +1,9 @@
 package com.omp.user;
 
 import com.omp.user.dto.CreateUserRequest;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,9 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/users")
 public class UserController {
     private final UserService userService;
+    private static final String USER_URI = "/api/v1/users/";
 
     @GetMapping("/{id}")
     public User getUser(final @PathVariable Long id) {
@@ -21,7 +24,9 @@ public class UserController {
     }
 
     @PostMapping
-    public Long createUser(final @RequestBody CreateUserRequest request) {
-        return userService.saveUserBy(CreateUserRequest.from(request));
+    public ResponseEntity<Void> createUser(final @RequestBody CreateUserRequest request) {
+        return ResponseEntity
+                .created(URI.create(USER_URI + userService.saveUserBy(CreateUserRequest.from(request))))
+                .build();
     }
 }
